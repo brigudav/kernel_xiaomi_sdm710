754e7bb04f4235f87b2244673818ab3b1ef3c25e
brigudav <brigudav@gmail.com>
2020-09-28 15:43:45 +1000

    drivers : input : drop touchscreen tests drivers

--- drivers/input/touchscreen/focaltech_touch/focaltech_config.h
+++ drivers/input/touchscreen/focaltech_touch/focaltech_config.h
@@ -102,7 +102,7 @@
  * show debug log info
  * enable it for debug, disable it for release
  */
-#define FTS_DEBUG_EN                            1
+#define FTS_DEBUG_EN                            0
 
 /*
  * Linux MultiTouch Protocol
@@ -127,12 +127,6 @@
  * default: disable
  */
 #define FTS_ESDCHECK_EN                         0
-
-/*
- * Production test enable
- * 1: enable, 0:disable(default)
- */
-#define FTS_TEST_EN                             1
 
 /*
  * Glove mode enable
